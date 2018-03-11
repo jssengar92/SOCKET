5d5ea78abbbd441fd0e3133c6a7341d48edc3823
Jyoti Singh <jssengar92@gmail.com>
2018-03-11 13:41:44 +0530

    commiting header

---
+++ AF_UNIX/header.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<errno.h>
+#include<sys/un.h>
+#include<unistd.h>
+#include<fcntl.h>
+
